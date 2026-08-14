e1b2112239c07e1db568712d7219e95921b2eb81
Fran Barton <francis.barton@nhs.net>
2026-08-14 15:11:43 +0100

    :memo: Update function and package documentation

--- dev/generate_manifest.R
+++ dev/generate_manifest.R
@@ -5,8 +5,7 @@ files <- c(
   "NAMESPACE",
   "app.R",
   fs::dir_ls("R"),
-  fs::dir_ls("inst", recurse = TRUE, type = "file"),
-  fs::dir_ls("supporting_data", recurse = TRUE, type = "file")
+  fs::dir_ls("inst", recurse = TRUE, type = "file")
 )
 
 rsconnect::writeManifest(appFiles = files)
